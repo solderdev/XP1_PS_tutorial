ede03d852200818c0c0cc061b0da09c8075f65bc
Peter Sammer <sammer.p@gmail.com>
2017-03-15 16:30:41 +0100

    [PS] added tests for calculations

--- Calculator/app/src/test/java/at/sw2017/calculator/CalculatorUnitTest.java
+++ Calculator/app/src/test/java/at/sw2017/calculator/CalculatorUnitTest.java
@@ -14,4 +14,43 @@ public class CalculatorUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void testDoAddition() {
+        int result = Calculations.doAddition(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testDoSubtraction() {
+        int result = Calculations.doSubtraction(7, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testDoMultiplication() {
+        int result = Calculations.doMultiplication(2, 3);
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void testDoDivision() {
+        int result = Calculations.doDivision(8, 4);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDoDivision1() {
+        int result = Calculations.doDivision(8, 0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testDoDivision2() {
+        int result = Calculations.doDivision(11, 4);
+        assertEquals(2, result);
+    }
+
+
+
 }
