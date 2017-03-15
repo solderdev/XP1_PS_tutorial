1e8f8958317d08767114020328838e7fa6ac768c
Peter Sammer <sammer.p@gmail.com>
2017-03-15 14:47:54 +0100

    [PS, MT] first test run and fail

--- Calculator/app/src/androidTest/java/at/sw2017/calculator/CalculatorInstrumentedTest.java
+++ Calculator/app/src/androidTest/java/at/sw2017/calculator/CalculatorInstrumentedTest.java
@@ -2,11 +2,16 @@ package at.sw2017.calculator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -16,10 +21,25 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class CalculatorInstrumentedTest {
+    @Rule
+    public ActivityTestRule<Calculator> mActivityRule = new ActivityTestRule <>(Calculator.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("at.sw2017.calcuator", appContext.getPackageName());
+    }
+
+    @Test
+    public void testButtons() throws Exception {
+        for (int i = 0; i <= 9; i++) {
+            onView(withText(Integer.toString(i))).perform(click());
+        }
+        onView(withText("+")).perform(click());
+        onView(withText("-")).perform(click());
+        onView(withText("*")).perform(click());
+        onView(withText("/")).perform(click());
+        onView(withText("=")).perform(click());
     }
 }
